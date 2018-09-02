4c26cef0763837cc03a38bfa7e25e08a718c8bfd
Arda Çetinkaya <ctnkyrd@gmail.com>
2018-09-02 13:04:22 +0300

    app js port ip

--- app.js
+++ app.js
@@ -63,6 +63,10 @@ function addAdmin(){
 }
 
 
-app.listen(process.env.PORT || 3000, process.env.IP || 'localhost', function(){
-   console.log("The frpApp Server Started!");
-});
+// app.listen(process.env.PORT || 3000, process.env.IP || 'localhost', function(){
+//    console.log("The frpApp Server Started!");
+// });
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The frpApp Server Started!");
+ });
