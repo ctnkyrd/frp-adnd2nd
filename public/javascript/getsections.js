9ebaf1e7e8bd8551ed9fdfba475e4cfd45a81a17
Arda Çetinkaya <ctnkyrd@gmail.com>
2018-09-10 22:54:47 +0300

    arrange tidy up get section details

--- public/javascript/getsections.js
+++ public/javascript/getsections.js
@@ -1,19 +1,24 @@
 // A $( document ).ready() block.
 $(document).ready(function () {
-    // $( "#pills-home-tab" ).click(function() {
-    //     var gameId = window.location.href.split('//')[1].split('/')[2].replace('?', '');
-    //     $.get('/games/'+gameId+'/sections', function (data) {
-    //         debugger;
-    //     });
-    // });     
+
+    if(!$('#section-select select') === null){
+        var sectionItem = $('#section-select select').val();
+        var gameId = window.location.href.split('//')[1].split('/')[2].replace('?', '');
+        getSectionDetails(sectionItem, gameId);
+    }
+
 
     $('#section-select select').on('change', function(){
         var sectionItem = $(this).val();
         var gameId = window.location.href.split('//')[1].split('/')[2].replace('?', '');
+        getSectionDetails(sectionItem, gameId);
+    });
+
+    function getSectionDetails(sectionId, gameId){
         $('#section-content h2').text('');
         //make get request to induvidual section and get date/description and all user comments also show comment editor
-        $.get('/games/'+gameId+'/sections/'+sectionItem, function(data){
+        $.get('/games/'+gameId+'/sections/'+sectionId, function(data){
             $('#section-content h2').text(data.name + "-" + moment(data.startdate).locale("tr").format('LL'));
         });
-    });
+    }
 });
